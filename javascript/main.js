0094f6bec68166a4a08a60d16e5581a1efd8e4d4
Linus Hernvall <linus.hernvall@gmail.com>
2022-11-23 11:37:30 +0100

    Started with header for mobile.

--- javascript/main.js
+++ javascript/main.js
@@ -2,4 +2,14 @@ window.addEventListener('DOMContentLoaded', main);
 
 function main() {
     addEventListeners();
-}
+  }
+  
+  function addEventListeners() {
+    const burgerMenuButton = document.getElementById('burger-menu');
+    burgerMenuButton.onclick = toggleMenu;
+  }
+  
+  function toggleMenu() {
+    const header = document.querySelector('header');
+    header.classList.toggle('open-header');
+  }
