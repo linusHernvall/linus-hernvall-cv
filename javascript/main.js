aa278e35211b97dd7c979c7415c8d6f8f43313b0
Linus Hernvall <linus.hernvall@gmail.com>
2022-11-23 17:44:33 +0100

    Kept working on dropdown.

--- javascript/main.js
+++ javascript/main.js
@@ -15,6 +15,16 @@ function main() {
     header.classList.toggle('open-header');
   }
 
+// function showDropdownExperience() {
+//     const dropdownExperience = document.querySelectorAll('.experience p');
+//     for (const p of dropdownExperience){
+//         dropdownExperience.addEventListener('p');
+
+//     }
+// }
+
+
+  /** Displays former work experience. */
   function showDropdownExperience() {
       const dropdownExperience = document.getElementById('company-name');
       dropdownExperience.onclick = toggleDropdown;
@@ -23,4 +33,5 @@ function main() {
   function toggleDropdown() {
       const workExperience = document.getElementById('dropdown');
       workExperience.classList.toggle('work-info');
+
   }
